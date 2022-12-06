8403bc00d21f45444eab0cac120a5e61f66d1ff6
user101a <brentnallshaun@gmail.com>
2022-12-06 19:19:57 +0000

    fix: bash script

--- element.sh
+++ element.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
+re='^[0-9]+$' 
+
+if [[ $1 =~ $re ]];
+then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $1")
+  echo "$ELEMENT"
+elif [ ${#1} -le 2 ] && [ ${#1} -gt 0 ]; then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$1'")
+  echo "$ELEMENT"
+elif [ ${#1} -gt 2 ]; then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE name = '$1'")
+  echo "$ELEMENT"
+else
+  echo "Please provide an element as an argument."
+fi 
